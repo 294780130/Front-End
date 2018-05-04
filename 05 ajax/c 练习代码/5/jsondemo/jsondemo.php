406fcdaf5aa901c0e8ce4986c37e2d1eb9736cb4
294780130 <294780130@qq.com>
2018-05-04 16:16:48 +0800

    mobile change

--- 05 ajax/c 练习代码/5/jsondemo/jsondemo.php
+++
@@ -1,9 +0,0 @@
-<?php
-$user = $_POST['user'];
-$json_user = json_decode($user,true);
-
-//$json = '{"username":"lisi","age":"20"}';
-
-echo json_encode($json_user);
-
-?>
--- 05 ajax/c 练习代码/5/xmldemo/xmldemo.php
+++
@@ -1,9 +0,0 @@
-<?php
-$user = $_POST['user'];
-$doc = new DOMDocument();
-$result = $doc->loadXML($user);
-
-header('Content-Type:application/xml');
-echo $doc->saveXML();
-
-?>
